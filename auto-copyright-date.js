afa265b077da9ccd54a4b353016443d0275e0f5e
Homer Gaines <xirclebox@gmail.com>
2016-04-02 17:56:37 -0400

    Update auto-copyright-date.js

--- auto-copyright-date.js
+++ auto-copyright-date.js
@@ -1,9 +1,15 @@
+//HTML
+//The static date is in place in the event the user has js turned off
+<div class="copyright" aria-label="copyright">&copy; 2001 - <span class="yr">2016</span> YOUR NAME OR COMPANY NAME</div>
+
 // Plain JavaScript 
 var thisYear = new Date();
 var copyRightYr = thisYear.getFullYear();
-document.getElementById('YOUR ELEMENT ID').innerHTML="© 2010 - " + copyRightYr + " YOUR NAME";
+var yrSpan = document.getElementsByClassName('yr')[0]
+yrSpan.innerHTML = copyRightYr;
+
 
 // Using jQuery
 var thisYear = new Date();
 var copyRightYr = thisYear.getFullYear();
-$('YOUR ELEMENT ID OR CLASS').html("© 2010 - " + copyRightYr + " YOUR NAME");
+$('.yr').html(copyRightYr);
