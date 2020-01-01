4953a177092ed210e481ca1b3ccf764eb02ed2e8
Homer Gaines <xirclebox@gmail.com>
2020-01-01 17:53:35 -0500

    Update auto-copyright-date.js

--- auto-copyright-date.js
+++ auto-copyright-date.js
@@ -1,7 +1,6 @@
 //HTML
-//The static date is in place in the event the user has js turned off
 <div class="copyright">
-  <p>&copy; 2001 - <span class="current-yr">2020</span> YOUR NAME OR COMPANY NAME</p>
+  <p>&copy; 2001 - <span class="current-yr"></span> YOUR NAME OR COMPANY NAME</p>
 </div>
 
 // Plain JavaScript 
