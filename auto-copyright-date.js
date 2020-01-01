4ecd52a7bc9d742123a9163086989fe93939e29a
Homer Gaines <xirclebox@gmail.com>
2020-01-01 17:44:43 -0500

    Update auto-copyright-date.js

--- auto-copyright-date.js
+++ auto-copyright-date.js
@@ -1,15 +1,17 @@
 //HTML
 //The static date is in place in the event the user has js turned off
-<div class="copyright" aria-label="copyright"><p>&copy; 2001 - <span class="yr">2016</span> YOUR NAME OR COMPANY NAME</p></div>
+<div class="copyright">
+  <p>&copy; 2001 - <span class="current-yr">2020</span> YOUR NAME OR COMPANY NAME</p>
+</div>
 
 // Plain JavaScript 
 var thisYear = new Date();
 var copyRightYr = thisYear.getFullYear();
-var yrSpan = document.getElementsByClassName('yr')[0]
+var yrSpan = document.getElementsByClassName('current-yr')[0]
 yrSpan.innerHTML = copyRightYr;
 
 
 // Using jQuery
 var thisYear = new Date();
 var copyRightYr = thisYear.getFullYear();
-$('.yr').html(copyRightYr);
+$('.current-yr').html(copyRightYr);
